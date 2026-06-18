16d6df37613ffb2750d7271d7304d31aba634662
Blair Hudson <blairhudson@users.noreply.github.com>
2026-06-18 17:58:39 +1000

    Add shared docs nav and community links

---
+++ docs/src/content/site-links.ts
@@ -0,0 +1,3 @@
+export const siteUrl = "https://openremote.blairhudson.com";
+export const repoUrl = "https://github.com/blairhudson/openremote";
+export const discordUrl = "https://discord.gg/3XqeTpcvqh";
